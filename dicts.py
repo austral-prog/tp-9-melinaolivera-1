eb8d029ced6da875a67009cf8f9cf6ffb9df304b
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-10-28 19:12:46 -0300

    Update dicts.py

--- dicts.py
+++ dicts.py
@@ -1,3 +1,4 @@
+
 def create_inventory(items):
     diccionario = dict()
     for item in items:
@@ -9,11 +10,6 @@ def create_inventory(items):
 
 lista = (["coal", "wood", "wood", "diamond", "diamond", "diamond"])
 print(create_inventory(lista))
-
-
-
-
-
 def add_items(inventory, items):#el diccionario ya fue creado y se llama inventory
     for item in items:
         if item in inventory:
@@ -22,20 +18,15 @@ def add_items(inventory, items):#el diccionario ya fue creado y se llama invento
             inventory[item] = 1
     return inventory
 
-
-
 def decrement_items(inventory, items):
     for item in items:
-        if item in inventory:
+        if item in inventory and inventory[item] > 0:
             inventory[item] -= 1
-        else:
-            inventory[item] = 1
     return inventory
 
 diccionario= {"coal":3, "diamond":1, "iron":5}
 lista = ["diamond", "coal", "iron", "iron"]
 print(decrement_items(diccionario, lista))
-
 
 def remove_item(inventory, item):
     if item in inventory:
@@ -45,13 +36,9 @@ diccionario = {"coal":2, "wood":1, "diamond":2}
 palabra = "coal"
 print(remove_item(diccionario, palabra))
 
-
-
 def list_inventory(inventory):
    resultado = []
    for key, value in inventory.items():
        if value > 0:
-           resultado.append(key,value)
+           resultado.append((key, value))# si pongo un solo parentesis pareceria que estoy pasasdo dos argumentos por separado
    return resultado
-
-
